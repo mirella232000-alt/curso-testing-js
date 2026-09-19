23faa63448e32abb84b2403d7912d6e2218b315e
Ana Mirella Saavedra pulachr <ana@MacBook-Air-de-Ana.local>
2026-09-18 19:40:23 -0500

    add books e2e and integration tests

---
+++ api/e2e/books.e2e.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+const { MongoClient } = require('mongodb');
+
+const createApp = require('../src/app');
+const { config } = require('../src/config');
+
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
+
+describe('Test for books', () => {
+  let app = null;
+  let server = null;
+  let database = null;
+
+  beforeAll(async () => {
+    app = createApp();
+    server = app.listen(3001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('test for [GET] /api/v1/books', () => {
+    test('should return a list books', async () => {
+      // Arrange
+      await database.collection('books').deleteMany({});
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'book1',
+          year: 1998,
+          author: 'nicolas',
+        },
+        {
+          name: 'book2',
+          year: 1998,
+          author: 'nicolas',
+        },
+      ]);
+      console.log(seedData);
+      // Act
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          // Assert
+          expect(body.length).toEqual(seedData.insertedCount);
+        });
+    });
+  });
+});
